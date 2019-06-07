4b9b4c6ab5855fd0fd0cd0806302a18a54dfb15b
Devin Helmgren <devincollege@gmail.com>
2019-06-07 11:16:11 -0700

    made the changes to swap state

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,65 +1,100 @@
-import React from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import './styles/Playlist.css';
 
 import Track from './Track';
 
-const calculatePlayTime = (tracks) => {
-  let minutes = 0;
-  let seconds = 0;
-  tracks.forEach((track) => {
-    const times = track.playtime.split(':');
-    minutes += parseInt(times[0]);
-    seconds += parseInt(times[1]);
-  });
+class Playlist extends Component {
 
-  minutes += Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+  propTypes = {
+    tracks: PropTypes.array,
+    side: PropTypes.string,
+    toggleFavorite: PropTypes.func,
+    sendToTop: PropTypes.func,
+  }
 
-  seconds %= 60;
-  minutes %= 60;
+  constructor(props) {
+    super(props);
 
-  seconds = ("" + seconds).padStart(2, "0");
-  minutes = ("" + minutes).padStart(2, "0");
+    this.state = {
+      tracks: props.tracks
+    }
 
-  return `${hours}:${minutes}:${seconds}`;
-}
+  }
 
-const Playlist = (props) => {
-  const tracks = props.tracks;
-  const trackCount = tracks.length;
-  const playtime = calculatePlayTime(tracks);
-  const trackElements = tracks.map((track, i) => {
+  calculatePlayTime = (tracks) => {
+    let minutes = 0;
+    let seconds = 0;
+    tracks.forEach((track) => {
+      const times = track.playtime.split(':');
+      minutes += parseInt(times[0]);
+      seconds += parseInt(times[1]);
+    });
+
+    minutes += Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+
+    seconds %= 60;
+    minutes %= 60;
+
+    seconds = ("" + seconds).padStart(2, "0");
+    minutes = ("" + minutes).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  toggleFavorite(index) {
+    // ugh where's Redux when you need it?
+    const newState = this.state;
+    const playlistTracks = newState.tracks;
+    const track = playlistTracks[index];
+
+    track.favorite = !track.favorite;
+
+    playlistTracks[index] = track;
+    newState.tracks = playlistTracks;
+    this.setState(newState);
+  }
+
+  sendToTop(index) {
+    const newState = this.state;
+    const playlistTracks = newState.tracks;
+
+    const track = playlistTracks[index];
+    playlistTracks.splice(index, 1);
+    playlistTracks.unshift(track);
+
+    newState.tracks = playlistTracks;
+    this.setState(newState);
+  }
+
+  render() {
+    const tracks = this.state.tracks;
+    const trackCount = tracks.length;
+    const playtime = this.calculatePlayTime(tracks);
+    const trackElements = tracks.map((track, i) => {
+      return (
+        <Track
+          key={i}
+          {...track}
+          toggleFavorite={() => this.toggleFavorite(i)}
+          sendToTop={() => this.sendToTop(i)}
+        />
+      );
+    });
+
     return (
-      <Track
-        key={i}
-        {...track}
-        toggleFavorite={() => props.toggleFavorite(i)}
-        sendToTop={() => props.sendToTop(i)}
-        switchLists={() => props.switchLists(i)}
-      />
+      <div className="playlist">
+        <h2>{this.props.side} Playlist</h2>
+        <p>
+          {trackCount} tracks - {playtime}
+        </p>
+        <ul className="playlist--track-list">
+          {trackElements}
+        </ul>
+      </div>
     );
-  });
-
-  return (
-    <div className="playlist">
-      <h2>{props.side} Playlist</h2>
-      <p>
-        {trackCount} tracks - {playtime}
-      </p>
-      <ul className="playlist--track-list">
-        {trackElements}
-      </ul>
-    </div>
-  );
-}
-
-Playlist.propTypes = {
-  tracks: PropTypes.array,
-  side: PropTypes.string,
-  toggleFavorite: PropTypes.func,
-  sendToTop: PropTypes.func,
-  switchLists: PropTypes.func
+  }
 }
 
 export default Playlist;
--- src/components/RadioSet.js
+++ src/components/RadioSet.js
@@ -1,81 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./styles/RadioSet.css";
 
 import Playlist from './Playlist';
 
-class RadioSet extends Component {
-  constructor(props) {
-    super();
 
-    this.state = {
-      morningTracks: props.tracks.slice(0, props.tracks.length / 2),
-      eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
-    }
+  // constructor(props) {
+  //   super();
 
-  }
+  //   this.state = {
+  //     morningTracks: props.tracks.slice(0, props.tracks.length / 2),
+  //     eveningTracks: props.tracks.slice(props.tracks.length / 2, props.tracks.length)
+  //   }
 
-  toggleFavorite(index, playlist) {
-    // ugh where's Redux when you need it?
-    const newState = {...this.state};
-    const playlistTracks = [...newState[playlist]];
-    const track = {...playlistTracks[index]};
+  // }
 
-    track.favorite = !track.favorite;
-
-    playlistTracks[index] = track;
-    newState[playlist] = playlistTracks;
-    this.setState(newState);
-  }
-
-  sendToTop(index, playlist) {
-    const newState = {...this.state};
-    const playlistTracks = [...newState[playlist]];
-
-    const track = playlistTracks[index];
-    playlistTracks.splice(index, 1);
-    playlistTracks.unshift(track);
-
-    newState[playlist] = playlistTracks;
-    this.setState(newState);
-  }
-
-  switchLists(index, fromPlaylist, toPlaylist) {
-    const newState = {...this.state};
+  // switchLists(index, fromPlaylist, toPlaylist) {
+  //   const newState = {...this.state};
     
-    const fromPlaylistTracks = [...newState[fromPlaylist]];
-    const track = fromPlaylistTracks[index];
-    fromPlaylistTracks.splice(index, 1);
+  //   const fromPlaylistTracks = [...newState[fromPlaylist]];
+  //   const track = fromPlaylistTracks[index];
+  //   fromPlaylistTracks.splice(index, 1);
 
-    const toPlaylistTracks = [...newState[toPlaylist]];
-    toPlaylistTracks.unshift(track);
+  //   const toPlaylistTracks = [...newState[toPlaylist]];
+  //   toPlaylistTracks.unshift(track);
 
-    newState[fromPlaylist] = fromPlaylistTracks;
-    newState[toPlaylist] = toPlaylistTracks;
-    this.setState(newState);
-  }
-
-  render() {
-    return (
-      <div className="radio-set">
-        <section className="radio-set--playlist-container">
-          <Playlist
-            side="Morning"
-            tracks={this.state.morningTracks}
-            toggleFavorite={index => this.toggleFavorite(index, "morningTracks")}
-            sendToTop={index => this.sendToTop(index, "morningTracks")}
-            switchLists={index => this.switchLists(index, "morningTracks", "eveningTracks")}
-          />
-          <Playlist
-            side="Evening"
-            tracks={this.state.eveningTracks}
-            toggleFavorite={index => this.toggleFavorite(index, "eveningTracks")}
-            sendToTop={index => this.sendToTop(index, "eveningTracks")}
-            switchLists={index => this.switchLists(index, "eveningTracks", "morningTracks")}
-          />
-        </section>
-      </div>
-    );
-  }
+  //   newState[fromPlaylist] = fromPlaylistTracks;
+  //   newState[toPlaylist] = toPlaylistTracks;
+  //   this.setState(newState);
+  // }
+const RadioSet = (props) => {
+  return (
+    <div className="radio-set">
+      <section className="radio-set--playlist-container">
+        <Playlist
+          side="Morning"
+          tracks={props.tracks.slice(0, props.tracks.length / 2)}
+        />
+        <Playlist
+          side="Evening"
+          tracks={props.tracks.slice(props.tracks.length / 2, props.tracks.length)}
+        />
+      </section>
+    </div>
+  );
 }
+
 
 export default RadioSet;
